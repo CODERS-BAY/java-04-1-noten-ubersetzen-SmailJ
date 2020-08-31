181087091770412cffd7ed8f9a55e362afae4786
Smail <smail.jusic16@gmail.com>
2020-08-31 09:46:40 +0200

    Überarbeitet & Java14 Schreibweise hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -9,37 +9,61 @@ public class Main {
         System.out.println("Gib eine Note zwischen 1 & 5 ein");
         grade = sc.nextInt();
 
-        if(grade >= 1 && grade <= 5) {
+        if (grade >= 1 && grade <= 5) {
             switch (grade) {
                 case 1:
-                    System.out.println("Sehr gut"); break;
+                    System.out.println("Sehr gut");
+                    break;
                 case 2:
-                    System.out.println("Gut"); break;
+                    System.out.println("Gut");
+                    break;
                 case 3:
-                    System.out.println("Befriedigend"); break;
+                    System.out.println("Befriedigend");
+                    break;
                 case 4:
-                    System.out.println("Genügend"); break;
+                    System.out.println("Genügend");
+                    break;
                 case 5:
-                    System.out.println("Nicht Genügend"); break;
+                    System.out.println("Nicht Genügend");
+                    break;
             }
-        } else{
+        } else {
             System.out.println("Keine gültige Zahl");
         }
 
         // ODER SO
         switch (grade) {
             case 1:
-                System.out.println("Sehr gut"); break;
+                System.out.println("Sehr gut");
+                break;
             case 2:
-                System.out.println("Gut"); break;
+                System.out.println("Gut");
+                break;
             case 3:
-                System.out.println("Befriedigend"); break;
+                System.out.println("Befriedigend");
+                break;
             case 4:
-                System.out.println("Genügend"); break;
+                System.out.println("Genügend");
+                break;
             case 5:
-                System.out.println("Nicht Genügend"); break;
+                System.out.println("Nicht Genügend");
+                break;
             default:
                 System.out.println("ERROR");
+        }
+
+        //Neue Java14 Schreibweise
+        switch (grade) {
+            case 1 -> System.out.println("Sehr gut");
+            case 2 -> System.out.println("Gut");
+
+            case 3 -> System.out.println("Befriedigend");
+
+            case 4 -> System.out.println("Genügend");
+
+            case 5 -> System.out.println("Nicht Genügend");
+
+            default ->System.out.println("ERROR");
         }
 
     }
