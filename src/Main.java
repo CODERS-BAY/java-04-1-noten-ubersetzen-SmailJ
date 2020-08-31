c86a1b42c2ad38e32827b5ffe34e43598c14ca81
Smail <smail.jusic16@gmail.com>
2020-08-31 09:21:31 +0200

    Ready

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int grade;
+
+        System.out.println("Gib eine Note zwischen 1 & 5 ein");
+        grade = sc.nextInt();
+
+        if(grade >= 1 && grade <= 5) {
+            switch (grade) {
+                case 1:
+                    System.out.println("Sehr gut"); break;
+                case 2:
+                    System.out.println("Gut"); break;
+                case 3:
+                    System.out.println("Befriedigend"); break;
+                case 4:
+                    System.out.println("Genügend"); break;
+                case 5:
+                    System.out.println("Nicht Genügend"); break;
+            }
+        } else{
+            System.out.println("Keine gültige Zahl");
+        }
+
+        // ODER SO
+        switch (grade) {
+            case 1:
+                System.out.println("Sehr gut"); break;
+            case 2:
+                System.out.println("Gut"); break;
+            case 3:
+                System.out.println("Befriedigend"); break;
+            case 4:
+                System.out.println("Genügend"); break;
+            case 5:
+                System.out.println("Nicht Genügend"); break;
+            default:
+                System.out.println("ERROR");
+        }
+
+    }
+
+}
